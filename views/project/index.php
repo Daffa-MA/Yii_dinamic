61fd6cb9162f1659c057972efe4b62e9de2ace22
ChristopherAldoNatanael <christophernatanael123@gmail.com>
2026-05-18 10:54:16 +0700

    Enhancement: Remove 'View Profile' link from account actions in project index page to streamline user interface and focus on password management.

--- views/project/index.php
+++ views/project/index.php
@@ -1251,7 +1251,6 @@ $activeProjectDatabase = ($activeProject !== null && isset($projectDatabases[(in
                             </div>
 
                             <div class="account-actions">
-                                <a href="<?= Url::to(['site/profile']) ?>" class="account-action primary">View Profile</a>
                                 <a href="#" class="account-action" data-open-password-modal>Ubah Password</a>
                                 <?= Html::beginForm(['site/logout'], 'post', ['style' => 'margin:0;']) ?>
                                     <button type="submit" class="account-action" style="width:100%;">Logout</button>
